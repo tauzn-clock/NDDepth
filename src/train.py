a6837030260871e3756dcf6ad170a66619902d70
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2024-11-18 08:34:10 +0000

    update train

--- src/train.py
+++ src/train.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from model import Model, ModelConfig
 from dataloader import ImageDataset, preprocess_transform
-from loss import silog_loss, histogram_intersection_loss
+from loss import silog_loss, histogram_intersection_loss, get_metrics
 
 torch.manual_seed(42)
 
@@ -20,21 +20,51 @@ train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True)
 test_dataset = ImageDataset('/scratchdata/nyu_data', '/scratchdata/nyu_data/data/nyu2_test.csv', transform=preprocess_transform)
 test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=True)
 
-config =  ModelConfig("tiny07")
+config =  ModelConfig("micro07")
 model = Model(config).to("cuda")
 
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-for i, x in enumerate(tqdm.tqdm(train_dataloader)):
-    
-    for k in x.keys():
-        x[k] = x[k].to("cuda")
+for epoch in range(50):
+    model.train()
+    running_loss = 0.0
+
+    cnt = 0
+    for i, x in enumerate(tqdm.tqdm(train_dataloader)):
+        cnt += 1
+        for k in x.keys():
+            x[k] = x[k].to("cuda")
+            
+        d1, d2 = model(x)
         
-    d1, d2 = model(x)
+        gt = x["depth_values"]
+        del x
+        d1 = F.interpolate(d1[-1], size=gt.shape[2:], mode='bilinear', align_corners=False)
+        d2 = F.interpolate(d2[-1], size=gt.shape[2:], mode='bilinear', align_corners=False)
+        
+        loss = silog_loss(d1, gt) + histogram_intersection_loss(d2, gt) * 10
+        
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+        
+        if cnt == 100:
+            break
     
-    gt = x["depth_values"]
-    del x
-    d1 = F.interpolate(d1[-1], size=gt.shape[2:], mode='bilinear', align_corners=False)
-    d2 = F.interpolate(d2[-1], size=gt.shape[2:], mode='bilinear', align_corners=False)
+    print(f"Epoch {epoch} Loss: {running_loss / len(train_dataloader)}")
     
-    loss = silog_loss(d1, gt) + silog_loss(d2, gt) * 10
+    model.eval()
+    for i, x in enumerate(tqdm.tqdm(test_dataloader)):
+        for k in x.keys():
+            x[k] = x[k].to("cuda")
+            
+        d1, d2 = model(x)
+        
+        gt = x["depth_values"]
+        del x
+        d1 = F.interpolate(d1[-1], size=gt.shape[2:], mode='bilinear', align_corners=False)
+        d2 = F.interpolate(d2[-1], size=gt.shape[2:], mode='bilinear', align_corners=False)
+        d = (d1 + d2) /2
+        
+        print(get_metrics(gt, d))
